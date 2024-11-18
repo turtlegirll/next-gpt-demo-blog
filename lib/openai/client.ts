6b65bf1afa638bb15bf19d2ece932a7fef99a001
evelynforkhands <lyubaevadaria@gmail.com>
2024-11-18 13:06:39 +0100

    init openai client

--- lib/openai/client.ts
+++ lib/openai/client.ts
@@ -1,1 +1,8 @@
-// create a client for talking to Azure OpenAI and export it
+import { AzureOpenAI } from "openai";
+
+const apiVersion = process.env.OPENAI_API_VERSION;
+const endpoint = process.env.AZURE_OPENAI_ENDPOINT;
+const apiKey = process.env.AZURE_OPENAI_API_KEY;
+const deployment = process.env.DEPLOYMENT;
+
+export const openaiClient = new AzureOpenAI({apiVersion, endpoint, apiKey, deployment})
