2a96620d57265c8f5f7f0f12cceb2dcc67b6dae6
agata_laptop <e12023151@student.tuwien.ac.at>
2024-11-19 11:20:55 +0100

    save secon step

--- app/lib/openai/client.ts
+++ app/lib/openai/client.ts
@@ -1,5 +1,9 @@
 import { AzureOpenAI } from "openai";
+import OpenAI from 'openai';
 
 const deployment = process.env.DEPLOYMENT;
+const apiKey= process.env['OPENAI_API_KEY'];
+const endpoint= process.env['OPENAI_API_ENDPOINT'];
+const apiVersion= process.env['OPENAI_API_VERSION'];
 
-export const openaiClient = new AzureOpenAI({deployment})
+export const openaiClient = new AzureOpenAI({deployment,apiKey,endpoint,apiVersion});
